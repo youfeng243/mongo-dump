d1b5e4c043cf74cb987cb0845de083972cac7d6e
youfeng <youfeng@haizhi.com>
2017-12-12 18:10:17 +0800

    工商数据dump

---
+++ mongodb-dump-gs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#enterprise_data_gov
+
+set -ex
+
+dump_path=/home/nfs/dump-export-tmp/mongodb-full-dump/
+target_path=/home/nfs/server-download-dir/mongodb-dump/mongodb-full-dump
+
+chmod +x mongodump
+mkdir -p ${dump_path}
+./mongodump -h 172.16.215.16:40042 -d app_data -c enterprise_data_gov  -u read -p read -o ${dump_path}
+mkdir -p ${target_path}
+
+cd ${dump_path}
+zip app_data.zip -r app_data/
+mv app_data.zip ${target_path}
+echo "完成dump"
