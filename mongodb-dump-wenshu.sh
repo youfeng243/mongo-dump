e91b6cdd004c086fdf0a5e860e6081bf2a121595
youfeng <youfeng@haizhi.com>
2018-01-11 21:29:34 +0800

    裁判文书dump脚本

--- mongodb-dump-wenshu.sh
+++ mongodb-dump-wenshu.sh
@@ -8,7 +8,7 @@ target_path=/home/nfs/server-download-dir/mongodb-dump/mongodb-full-dump
 
 chmod +x mongodump
 mkdir -p ${dump_path}
-./mongodump -h mongo6:40042 -d app_data -c judgement_wenshu  -u root -p haizhi@123 -o ${dump_path}
+./mongodump -h mongo6:40042 -d app_data -c judgement_wenshu --authenticationDatabase admin -u root -p haizhi@123 -o ${dump_path}
 mkdir -p ${target_path}
 
 cd ${dump_path}
