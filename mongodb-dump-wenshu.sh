b5f1dbebb0b609794df9ac87f168188ff1949eeb
youfeng <youfeng@haizhi.com>
2018-01-12 09:57:59 +0800

    数据导出

--- mongodb-dump-wenshu.sh
+++ mongodb-dump-wenshu.sh
@@ -8,9 +8,9 @@ target_path=/home/nfs/server-download-dir/mongodb-dump/mongodb-full-dump
 
 chmod +x mongodump
 mkdir -p ${dump_path}
-./mongodump -h 172.16.215.16:40042 -d app_data -c judgement_wenshu  -u work -p haizhi --gzip -o ${dump_path}
-./mongodump -h 172.16.215.16:40042 -d app_data -c land_project_selling  -u work -p haizhi --gzip -o ${dump_path}
-./mongodump -h 172.16.215.16:40042 -d app_data -c investment_events  -u work -p haizhi --gzip -o ${dump_path}
+./mongodump -h 172.16.215.16:40042 -d app_data -c judgement_wenshu  -u work -p haizhi  -o ${dump_path}
+./mongodump -h 172.16.215.16:40042 -d app_data -c land_project_selling  -u work -p haizhi -o ${dump_path}
+./mongodump -h 172.16.215.16:40042 -d app_data -c investment_events  -u work -p haizhi -o ${dump_path}
 # mkdir -p ${target_path}
 
 #cd ${dump_path}
