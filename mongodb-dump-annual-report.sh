8530f451b021d5866c91d35a4845e35c3c04ba8c
youfeng <youfeng@haizhi.com>
2018-01-18 20:28:58 +0800

    修改年报导出脚本

--- mongodb-dump-annual-report.sh
+++ mongodb-dump-annual-report.sh
@@ -4,12 +4,21 @@
 set -ex
 
 dump_path=/ssd/mongodb-dump/
+target_path=/home/nfs/server-download-dir/mongodb-dump/mongodb-full-dump
+target_dump_path=/home/nfs/dump-export-tmp/mongodb-full-dump/
 
 chmod +x mongodump
 mkdir -p ${dump_path}
-./mongodump -h 172.16.215.16:40042 -d app_data -c annual_reports  -u work -p haizhi -o ${dump_path}
-
+./mongodump -h 172.16.215.16:40042 -d app_data -c annual_reports  -u work -p haizhi -o ${dump_path} --query '{"$and": [{"_utime": {"$gte": "2018-01-18 17:00:00"}}]}'
+echo "dump数据完成.."
 
 cd ${dump_path}/app_data/
 zip annual_reports.zip -r *annual_reports*
+echo "数据压缩完成.."
+
+sshpass -p youfeng243. scp annual_reports.zip youfeng@cs5:${target_dump_path}
+echo "远程复制完成.."
+
+ssh -nf cs5 "cd ${target_dump_path}; mv ${target_dump_path}annual_reports.zip ${target_path}"
+echo "远程移动文件完成.."
 echo "完成dump"
