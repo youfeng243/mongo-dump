2b8455e76a9285328b39d4704798ad69a60f58a6
youfeng <youfeng@haizhi.com>
2018-01-18 19:58:41 +0800

    修改导出脚本

--- mongodb-dump-annual-report.sh
+++ mongodb-dump-annual-report.sh
@@ -3,15 +3,13 @@
 
 set -ex
 
-dump_path=/home/nfs/dump-export-tmp/mongodb-full-dump/
-target_path=/home/nfs/server-download-dir/mongodb-dump/mongodb-full-dump
+dump_path=/home/youfeng/mongodb-dump/
 
 chmod +x mongodump
 mkdir -p ${dump_path}
 ./mongodump -h 172.16.215.16:40042 -d app_data -c annual_reports  -u work -p haizhi -o ${dump_path}
-mkdir -p ${target_path}
+
 
 cd ${dump_path}/app_data/
 zip annual_reports.zip -r *annual_reports*
-mv annual_reports.zip ${target_path}
 echo "完成dump"
